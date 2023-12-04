c5c64a9d871f8452acbd9426c79b7018830dd1a4
minjyun3011 <grand3canyon10@gmail.com>
2023-12-04 17:41:14 +0900

    おまけまで完了

--- app.py
+++ app.py
@@ -1,39 +1,76 @@
+import time
+
 from db_config import Message
 
 def delete_message():
     msg_id = input("削除するメッセージのIDを入力してください > ")
-    msg = Message.get_by_id(msg_id)
-    if not msg.delete_instance():
-        print("削除に失敗しました。IDを再度確認してください。")
+    try:
+        msg = Message.get_by_id(msg_id)
+        msg.delete_instance()
+        print(f"{msg.user}さんのメッセージを削除しました。")
+    except Message.DoesNotExist:
+        print("指定されたIDのメッセージは存在しません。")
 
 def edit_message():
     msg_id = input("編集するメッセージのIDを入力してください > ")
-    msg = Message.get_by_id(msg_id)
-    print(f"{msg.id} {msg.user} {msg.content} {msg.pub_date}")
-    msg.content = input("新しいメッセージを入力してください > ")
-    msg.save()
-    return msg
+    try:
+        msg = Message.get_by_id(msg_id)
+        print(f"{msg.id} {msg.user} {msg.content} {msg.pub_date}")
+        time.sleep(1)
+        msg.content = input("新しいメッセージを入力してください > ")
+        msg.save()
+        print(f"{msg.user}さんのメッセージを編集しました。")
+    except Message.DoesNotExist:
+        print("指定されたIDのメッセージは存在しません。")
+
+def is_user_message_exist(user_name):
+    return Message.select().where(Message.user == user_name).exists()
 
 def main():
-    user_name = input("ユーザーネームを入力してください > ")
-    message = ""  # 繰り返し条件の初期化
-
     while True:
-        # for msg in Message.select():
-        #     print(f"{msg.id} {msg.user} {msg.content} {msg.pub_date}")
-
-        message = input("メッセージを入力してください > ")
-        if message == "\q":
-            break
-        
-        if message == "\d":
-            delete_message()
+        user_name = input("ユーザーネームを入力してください > ")
+        if user_name == "":
+            print("名前が未入力です。入力をやり直してください.")
             continue
+        message = ""  # 繰り返し条件の初期化
         
-        if message == "\e":
-            edit_message()
-        
-        Message.create(user=user_name, content=message)
+        while True:
+            message = input("メッセージか専用コマンド(\\e, \\d, \\q)を入力してください > ")
+            if message == "\\q":
+                break
+
+            if message == "\\d":
+                if is_user_message_exist(user_name):
+                    delete_message()
+                else:
+                    print("あなたはこのメッセージを削除できません")
+                time.sleep(1)
+                continue
+
+            if message == "\\e":
+                if is_user_message_exist(user_name):
+                    edit_message()
+                else:
+                    print("あなたはこのメッセージを編集できません")
+                time.sleep(1)
+                continue
+
+            if message.startswith("\\"):
+                time.sleep(1)
+                print("無効なコマンドです。正しいコマンドを入力してください.")
+                time.sleep(1)
+                continue
+
+            if message == "":
+                time.sleep(1)
+                print("メッセージが未入力です。1文字以上入力してください.")
+                time.sleep(1)
+                continue
+
+            Message.create(user=user_name, content=message)
+            print(f"{user_name}としてメッセージの入力が完了しました！")
+            time.sleep(1)
+        break
 
 
 
